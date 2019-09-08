9ced586a4dd15a056004935630c6eb3d89e05dec
Art <girukartem@gmail.com>
2019-09-09 00:05:34 +0300

    microfull

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-
+    $('.top-table__top-list').click(function (e) {
+        $(this).toggleClass('open-list');
+        $(this).find('.top-table__bottom').slideToggle('.open-table');
+    });
 });

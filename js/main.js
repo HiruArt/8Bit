24e94b90a43477d70fcaec90f0ecd7939b47f725
ARTEM <girukartem@gmail.com>
2019-09-09 17:45:47 +0300

    first full

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
-    $('.top-table__top-list').click(function (e) {
-        $(this).toggleClass('open-list');
-        $(this).find('.top-table__bottom').slideToggle('.open-table');
+    $('.top-table__top-item').click(function (e) {
+        console.log(123);
+        if($(this).hasClass('open-list')){
+            $(this).closest('.top-table__top-list').find('.top-table__bottom').slideUp();
+            $(this).removeClass('open-list');
+        }   else {
+            $(this).closest('.top-table__top-list').find('.top-table__bottom').slideDown();
+            $(this).addClass('open-list');
+        }
     });
+
+    $('.header__lang-link--active').click(function () {
+        $('.header__lang-popup').slideToggle();
+        $('.header__lang-link--active').toggleClass('lang-arrow');
+    });
+
+    // var coin1 = document.getElementById('coin1');
+    var coin2 = document.getElementById('coin2');
+    var coin3 = document.getElementById('coin3');
+    var coin4 = document.getElementById('coin4');
+    var coin5 = document.getElementById('coin5');
+    var coin6 = document.getElementById('coin6');
+    var coin7 = document.getElementById('coin7');
+    var coin8 = document.getElementById('coin8');
+
+    // var parallax1 = new Parallax(coin1);
+    var parallax2 = new Parallax(coin2);
+    var parallax3 = new Parallax(coin3);
+    var parallax4 = new Parallax(coin4);
+    var parallax5 = new Parallax(coin5);
+    var parallax6 = new Parallax(coin6);
+    var parallax7 = new Parallax(coin7);
+    var parallax8 = new Parallax(coin8);
+
+    var bLazy = new Blazy();
 });
+
+
+

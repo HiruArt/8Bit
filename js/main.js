44e54a67d6686c8d3bd0c274d2b7e6218b914638
ARTEM <girukartem@gmail.com>
2019-09-10 16:58:24 +0300

    qwerty

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $('.top-table__top-item').click(function (e) {
-        console.log(123);
         if($(this).hasClass('open-list')){
             $(this).closest('.top-table__top-list').find('.top-table__bottom').slideUp();
             $(this).removeClass('open-list');
@@ -14,24 +13,30 @@ $(document).ready(function () {
         $('.header__lang-popup').slideToggle();
         $('.header__lang-link--active').toggleClass('lang-arrow');
     });
+    $('.header__lang-popup-link').click(function () {
+        $('.header__lang-popup').slideToggle();
+        $('.header__lang-link--active').toggleClass('lang-arrow');
+    });
 
-    // var coin1 = document.getElementById('coin1');
-    var coin2 = document.getElementById('coin2');
-    var coin3 = document.getElementById('coin3');
-    var coin4 = document.getElementById('coin4');
-    var coin5 = document.getElementById('coin5');
-    var coin6 = document.getElementById('coin6');
-    var coin7 = document.getElementById('coin7');
-    var coin8 = document.getElementById('coin8');
+    if($(window).width() > 1150 ) {
+        // var coin1 = document.getElementById('coin1');
+        var coin2 = document.getElementById('coin2');
+        var coin3 = document.getElementById('coin3');
+        var coin4 = document.getElementById('coin4');
+        var coin5 = document.getElementById('coin5');
+        var coin6 = document.getElementById('coin6');
+        var coin7 = document.getElementById('coin7');
+        var coin8 = document.getElementById('coin8');
 
-    // var parallax1 = new Parallax(coin1);
-    var parallax2 = new Parallax(coin2);
-    var parallax3 = new Parallax(coin3);
-    var parallax4 = new Parallax(coin4);
-    var parallax5 = new Parallax(coin5);
-    var parallax6 = new Parallax(coin6);
-    var parallax7 = new Parallax(coin7);
-    var parallax8 = new Parallax(coin8);
+        // var parallax1 = new Parallax(coin1);
+        var parallax2 = new Parallax(coin2);
+        var parallax3 = new Parallax(coin3);
+        var parallax4 = new Parallax(coin4);
+        var parallax5 = new Parallax(coin5);
+        var parallax6 = new Parallax(coin6);
+        var parallax7 = new Parallax(coin7);
+        var parallax8 = new Parallax(coin8);
+    }
 
     var bLazy = new Blazy();
 });
